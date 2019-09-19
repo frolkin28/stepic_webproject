66aaf0e7822cfcc8a34d87aad408c944c87d96ec
FrolkinV <frolkin2801@gmail.com>
2019-09-19 11:34:44 +0300

    chnge init.sh for step 2

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def wsgi_app(environ, start_response):
     status = '200 OK'
     headers = [
-        ('Content-type', 'plain/text')
+        ('Content-type', 'text/plain')
     ]
     query = environ['QUERY_STRING'].split('&')
     body = bytes('\r\n'.join(query), encoding='utf-8')
