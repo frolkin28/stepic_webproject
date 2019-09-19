ec3082025b5ed6b36cd28d3ff15791755c1d78d3
FrolkinV <frolkin2801@gmail.com>
2019-09-19 22:30:52 +0300

    still trying

--- init.sh
+++ init.sh
@@ -8,15 +8,10 @@
 
 
 
-#sudo rm /etc/nginx/sites-enabled/default
-#sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-#sudo /etc/init.d/nginx restart
-
-#sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-#sudo /etc/init.d/gunicorn restart
-
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo /etc/init.d/gunicorn restart
+
