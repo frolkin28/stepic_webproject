67691a6a2cac96987ef9f14395a50f1fc8336147
FrolkinV <frolkin2801@gmail.com>
2019-09-19 12:37:09 +0300

    gh init

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+//sudo /etc/init.d/nginx restart
+sudo nginx -c /etc/nginx/sites-enabled/test.conf
 
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
