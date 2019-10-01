8664d31b3e735da2134905a40612b3de0d67057b
FrolkinV <frolkin2801@gmail.com>
2019-10-01 17:03:23 +0300

    fix

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
 def wsgi_app(environ, start_response):
     status = '200 OK'
     headers = [
-        ('Content-Type', 'text/plain')
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(data)))
     ]
     query = environ['QUERY_STRING'].split('&')
     body = bytes('\r\n'.join(query), encoding='utf-8')
