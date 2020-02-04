4cf6b0cb6676f64b021fd3bce265dd19a5b8cd2d
frolkin28 <frolkin2801@gmail.com>
2020-02-04 21:00:22 +0200

    Added models

---
+++ ask/qa/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-id')
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(null=True)
+    author = models.ForeignKey(User, related_name='question_author')
+    likes = models.ManyToManyField(User, related_name='question_likes')
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now=True)
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User, related_name='answer_author')
