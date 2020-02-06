85827832589e26a664bd201d76af0b207ec20ee6
frolkin28 <frolkin2801@gmail.com>
2020-02-06 23:39:55 +0000

    commit from site 2.3

--- init.sh
+++ init.sh
@@ -1,13 +1,15 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo rm /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
-sudo rm /etc/gunicorn.d/test
+sudo rm /etc/gunicorn.d/test
 sudo rm /etc/gunicorn.d/ask
-sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo /etc/init.d/gunicorn restart
 
 sudo /etc/init.d/mysql start
-mysql -uroot -e "create database qa"
-sudo /home/box/web/ask/manage.py syncdb
+mysql -uroot -e "create database if not exists qa"
+
+sudo /home/box/web/ask/manage.py makemigrations
+sudo /home/box/web/ask/manage.py migrate
